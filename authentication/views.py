6f5480da715665a9efb85f912e4dd6f0a6daeab2
sarahmatishoi <sarahmatishoi@gmail.com>
2021-10-21 13:07:45 +0300

    deploy

--- authentication/views.py
+++ authentication/views.py
@@ -11,7 +11,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes,force_text
-from . tokens import generate_token
+from .tokens import generate_token
 from django.core.mail import EmailMessage
 from django.utils.http import urlsafe_base64_decode
 
