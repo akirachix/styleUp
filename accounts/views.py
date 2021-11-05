9e1cfd108d2f4a50b59eb97c6d43bca6c7833f2a
sarahmatishoi <sarahmatishoi@gmail.com>
2021-11-05 12:28:07 +0300

    deploy

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.decorators import login_required
 from accounts.forms import ContactForm
 # Create your views here.
 
-@login_required
+# @login_required
 def home(request):
     return render(request , 'home.html')
 
